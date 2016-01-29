562698ab34801c38939976e409fc6130b3a0d30d
Sasha Bayan <sashabayan@gmail.com>
2016-01-28 21:42:24 -0800

    feat: add and render first react component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1> Danny likes class </h1>
+        <h2> jk </h2>
+      </div>
+    )
+  }
+})
+
+export default App;
