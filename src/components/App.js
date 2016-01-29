7c5fab44a30a64e7a7742f4053929cf23cae9f53
Sasha Bayan <sashabayan@gmail.com>
2016-01-28 23:07:54 -0800

    feat: render boilerplate todo to page

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+import * as todoActions from '../actions/todoActions';
 
 const App = React.createClass({
   render() {
-    console.log(this.props.todos);
+    const todos = this.props.todos.map((todo) => <h3> {todo.text} </h3>)
     return (
       <div>
         <h1> Danny likes class </h1>
         <h2> jk </h2>
+
+        <button onClick={ () => this.props.dispatch(todoActions.addTodo('ADD ME')) }> PRESS THIS TO ADD TODO </button>
+        {todos}
       </div>
     )
   }
